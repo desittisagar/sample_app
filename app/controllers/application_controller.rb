b56a927326123fb03b118d8fd5bf7d2174c1fdb1
desitti sagar <dsagar.smart@gmail.com>
2021-02-18 15:55:25 +0530

    Add a full_title helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
+#require "application_helper"
+#include ApplicationHelper
+
 class ApplicationController < ActionController::Base
+    
+    helper :application
     def index
         render html: "Hello, World!"
     end    
